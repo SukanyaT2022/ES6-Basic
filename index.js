5cbd9c03915f8c2ea03d89f6cca0713864cfb7f4
TiparpronS <Sukanyat2022a@gmail.com>
2023-04-12 10:23:44 -0400

    add

---
+++ index.js
@@ -0,0 +1,35 @@
+// function add(a,b){
+//     var a = 2
+//     var b = 3
+//     result = a + b ;
+//   alert("result");
+// }
+
+// const add =(a,b)=> a+b;
+
+
+
+//template literals
+// const name = "june"
+// const age = 12
+// console.log("Hello my name is " + name + " I am  " +age +" year old ")
+
+const name2 = "wawa"
+const color = "green"
+console.log(`My name is  ${ name2 }  I like  ${color } my favourite`);
+
+// spread opaeration combine arrY
+const arr1 =[1,2,3];
+const arr2 = [4,5,6];
+const combineArray = arr1.concat(arr2);
+console.log(combineArray)
+
+
+const arr3 =[1,2,3];
+const arr4 = [6,7,8];
+const combineArray2 = [...arr3,...arr4]
+console.log(combineArray2
+    )
+
+
+
